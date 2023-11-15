263c62b9a4e196e07aca15d9ad4cb35ac40f7eae
hdh9078 <hdh9071@naver.com>
2023-11-15 15:50:21 +0900

    test

--- src/signin.tsx
+++ src/signin.tsx
@@ -1,8 +1,17 @@
+import React from "react"
+import {Link} from "react-router-dom";
+
 export default function Signin() {
 
     return (
-        <>
-            <div>login</div>
-        </>
+        <div className="signinContainer">
+            <span className="signinTitle">Demure</span>
+            <div className="signinWrapper">
+                <input className="id"/>
+                <input className="password"/>
+                <button className="signBtn">로그인</button>
+                <Link to="/signup">회원가입</Link> | 비밀번호찾기
+            </div>
+        </div>
     )
 }
