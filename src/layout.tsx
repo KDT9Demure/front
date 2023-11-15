bfe111d4b13ab652f916eabcc6632554353f08cf
k-minyoung <alsdudsk12@naver.com>
2023-11-15 16:25:15 +0900

    [feat] 회원가입 폼

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "layout.css"
+import "./layout.css"
 
 export default function Layout() {
 
--- src/signup.tsx
+++ src/signup.tsx
@@ -1,8 +1,25 @@
+import React from "react"
+
 export default function Signup() {
 
     return (
-        <>
-            <div>signup</div>
-        </>
+
+        <div className="signupContainer">
+            <span className="signupTitle">Demure</span>
+            <form className="signupForm">
+
+                <input type="text" className="id" placeholder="ID" /><br />
+                <input type="password" className="password" placeholder="Password" /><br />
+                <input type="password" className="confirmPassword" placeholder="Confirm Password" /><br />
+                <input type="text" className="name" placeholder="Name" /><br />
+                <input type="email" className="email" placeholder="Email" /><br />
+                <button className="emailBtn">이메일 인증</button><br />
+                <input type="text" className="emailNumber" placeholder="Author Number" />
+                <button className="emailNumberConfirm">확인</button><br />
+                <button className="signupBtn">회원가입</button>
+
+            </form>
+        </div>
+
     )
 }
