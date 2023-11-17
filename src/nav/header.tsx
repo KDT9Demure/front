9af8e09437e16271e5b9847ccde31cd845cc7308
bliss1454 <rkgus1454@naver.com>
2023-11-17 20:56:29 +0900

    [Design] header 디자인

--- src/nav/header.tsx
+++ src/nav/header.tsx
@@ -1,55 +1,90 @@
 import styles from "../css/header.module.css";
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRightToBracket,faUser,faBasketShopping  } from "@fortawesome/free-solid-svg-icons";
+import { faRightToBracket, faUser, faBasketShopping, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
 
-    const [openMenu, setOpenMenu] = useState(false);
+  const onLogoClick = () => {
+    setOpenMenu(!openMenu);
+  };
 
-    const onClick = () => {
-        setOpenMenu(true);
-    };
+  const onClick2 = () => {
+    setOpenMenu(false);
+  }
 
-    const onClick2 = () => {
-        setOpenMenu(false);
-    }
-
-
-    return (
-        <div className={styles.container}>
-            {openMenu ? <div className={styles.wrapper} onClick={onClick2}>Demure
-                <div className={styles.menu} onClick={(e) => {e.stopPropagation()}}>
-                    <input placeholder="검색"/>
-                    <dt>shop
-                        <dd>
-                            의자
-                        </dd>
-                        <dd>
-                            책상
-                        </dd>
-                    </dt>
-                    <dt>customer service
-                        <dd>
-                            q&a
-                        </dd>
-                        <dd>
-                            문의
-                        </dd>
-                    </dt>
-                    <dt>event</dt>
-                </div>
-            </div> : <div className={styles.main} onClick={onClick}>Demure</div>}
-                <div>
-                    <FontAwesomeIcon icon={faRightToBracket} style={{ color: "#ffffff" }} />
-                    Bonjour!
-                </div>
-                <div>
-                    <FontAwesomeIcon icon={faUser} style={{color: "#ffffff",}} />
-                </div>
-                <div>
-                    <FontAwesomeIcon icon={faBasketShopping} style={{color: "#ffffff",}} />
-                </div>
+  return (
+    <div className={styles.container}>
+        <div className={styles.header_logo} onClick={onLogoClick}>
+          Demure
         </div>
-    )
+        <div className={styles.header_user}>
+            <div>
+                <FontAwesomeIcon icon={faRightToBracket} style={{ color: "#ffffff" }} />
+                Bonjour!
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faUser} style={{ color: "#ffffff" }} />
+            </div>
+            <div>
+                <FontAwesomeIcon icon={faBasketShopping} style={{ color: "#ffffff" }} />
+            </div>
+        </div>
+      {openMenu && (
+        <div className={styles.wrapper} onClick={onClick2}>
+            <div className={styles.menu} onClick={(e) => { e.stopPropagation() }}>
+                <span className={styles.header_search}>
+                    <FontAwesomeIcon className={styles.header_search_icon} icon={faMagnifyingGlass} style={{ color: "#ffffff" }} />
+                    <input type="text"className={styles.header_search_input} placeholder="What do you looking for?" />
+                </span>
+                <div className={styles.header_menu}>
+                    <dt>Category
+                        <dd>
+                            Bathroom products
+                        </dd>
+                        <dd>
+                            Kitchen
+                        </dd>
+                        <dd>
+                            Beds & mattresses
+                        </dd>
+                        <dd>
+                            Chairs
+                        </dd>
+                        <dd>
+                            Tables & desks
+                        </dd>
+                        <dd>
+                            Lighting
+                        </dd>
+                        <dd>
+                            Baby & Children
+                        </dd>
+                        <dd>
+                            Decoration
+                        </dd>
+                        <dd>
+                            Living room
+                        </dd>
+                        <dd>
+                            Cabinets
+                        </dd>
+                    </dt>
+                    <dt>Customer Service
+                        <dd>
+                            QnA
+                        </dd>
+                        <dd>
+                            Notice
+                        </dd>
+                    </dt>
+                    <dt>Event
+                    </dt>
+                </div>
+            </div>
+        </div>
+      )}
+    </div>
+  )
 }
