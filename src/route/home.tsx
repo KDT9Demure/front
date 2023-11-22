1d58bfb962d80d5bd3f0c961554f18433094c780
bliss1454 <rkgus1454@naver.com>
2023-11-22 20:54:13 +0900

    [FEAT] main page

--- src/route/home.tsx
+++ src/route/home.tsx
@@ -5,38 +5,42 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 
 type SlideImage = string[];
 
+const createCyclicArray = (arr: any[], times: number) => {
+    return Array.from({ length: times }, () => [...arr]).flat();
+};
+
 export default function Home() {
-    const [slideImg, setSlideImg] = useState<SlideImage>([]);
+    const initialSlides = [
+        'assets/slide1.jpg',
+        'assets/slide2.jpg',
+        'assets/slide3.jpg',
+        'assets/slide4.jpg',
+        'assets/slide5.jpg',
+        'assets/slide6.jpg',
+    ];
+
+    const [slideImg, setSlideImg] = useState<SlideImage>(createCyclicArray(initialSlides, 10));
     const [slideCount, setSlideCount] = useState<number>(0);
 
     useEffect(() => {
-        const slide1 = 'assets/slide1.jpg';
-        const slide2 = 'assets/slide2.jpg';
-        const slide3 = 'assets/slide3.jpg';
-        const slide4 = 'assets/slide4.jpg';
-        const slide5 = 'assets/slide5.jpg';
-        const slide6 = 'assets/slide6.jpg';
-
-        setSlideImg([slide1, slide2, slide3, slide4, slide5, slide6, slide5, slide4, slide3, slide2, slide1]);
-    }, []);
-
-    useEffect(() => {
         const interval = setInterval(() => {
-          setSlideCount((prevCount) => (prevCount + 1) % slideImg.length);
+            setSlideCount((prevCount) => (prevCount + 1) % slideImg.length);
+            setSlideImg((prevSlides) => {
+                return prevSlides;
+            });
         }, 2000);
-    
-        return () => clearInterval(interval);
 
+        return () => clearInterval(interval);
     }, [slideCount, slideImg]);
-    
+
     const handlePrev = () => {
         setSlideCount((prevCount) => (prevCount - 1 + slideImg.length) % slideImg.length);
     };
-    
+
     const handleNext = () => {
         setSlideCount((nextCount) => (nextCount + 1) % slideImg.length);
     };
-    
+
     const slideStyle: CSSProperties = {
         transform: `translate(-${slideCount * 100}%, 0px)`,
     };
@@ -50,18 +54,18 @@ export default function Home() {
                             <img className={homeStyles.slideBoxImg} key={index} src={value} alt={`slide${index}`} />
                         ))}
                     </div>
-                    <div className={homeStyles.slideBtnBox}>
-                        <FontAwesomeIcon
-                            icon={faChevronLeft}
-                            className={homeStyles.slideLeftBtn}
-                            onClick={handlePrev}
-                        />
-                        <FontAwesomeIcon
-                            icon={faChevronRight}
-                            className={homeStyles.slideRightBtn}
-                            onClick={handleNext}
-                        />
-                    </div>
+                </div>    
+                <div className={homeStyles.slideBtnBox}>
+                    <FontAwesomeIcon
+                        icon={faChevronLeft}
+                        className={homeStyles.slideLeftBtn}
+                        onClick={handlePrev}
+                    />
+                    <FontAwesomeIcon
+                        icon={faChevronRight}
+                        className={homeStyles.slideRightBtn}
+                        onClick={handleNext}
+                    />
                 </div>
             </section>
             <section className={homeStyles.mainBrand}>
