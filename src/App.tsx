a3385873f7ec363357eeb40acf5f0a9cc40236ae
Leewonno <dldnjssh123@naver.com>
2023-11-27 11:55:26 +0900

    [Design] Header Design 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,7 @@ import { Outlet } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-
-
 function App() {
-
-
   return (
     <>
       <Provider store={store}>
