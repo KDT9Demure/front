9560817131592d000ba99c964eac16ad2e8392dd
hdh9078 <hdh9071@naver.com>
2023-11-28 20:56:29 +0900

    [Feat] chatbot

---
+++ src/route/Chatbot.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPersonCircleQuestion } from "@fortawesome/free-solid-svg-icons";
+import styles from "../css/chatbot.module.css";
+
+
+
+export default function Chatbot() {
+    const [openChatbot, setOpenChatbot] = useState<boolean>(false);
+
+    return (
+        
+            <div className={styles.chatbotIconContainer}>
+                <FontAwesomeIcon icon={faPersonCircleQuestion} className={styles.chatbotIcon} onClick={() => setOpenChatbot(true)}/>
+            </div>
+    )
+}
