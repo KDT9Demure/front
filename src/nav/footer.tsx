9e621bf85453d9073c32fb3f7e26bf4ef4fb2811
bliss1454 <rkgus1454@naver.com>
2023-11-29 21:12:46 +0900

    [Design] footer

--- src/nav/footer.tsx
+++ src/nav/footer.tsx
@@ -9,10 +9,6 @@ export default function Footer() {
       <div className={footerStyles.footerContent}>
         <div className={footerStyles.companyInfo}>
           <div className={footerStyles.companyName}>Demure 2023.</div>
-          <div className={footerStyles.companyAddress}>5 Avenue, 75007 Paris France</div>
-          <div className={footerStyles.companyContact}>고객센터: +33 892 70 12 39</div>
-        </div>
-        <div className={footerStyles.projectMember}>
           <div className={footerStyles.projectTitle}>포스코X코딩온 웹 풀스택</div>
           <div className={footerStyles.projectMembers}>
             <div>이원노</div>
