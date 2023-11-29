8c655f29c230c53bab9b61209d9e2eab117d277e
hdh9078 <hdh9071@naver.com>
2023-11-30 04:43:19 +0900

    [Feat] 공지사항페이지 공지제작시 시간 전원 변경되는 것 수정

--- src/route/Notice.tsx
+++ src/route/Notice.tsx
@@ -4,10 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
 
-function Notions({ que, anw }: { que: string, anw: string }) {
+function Day() {
+    const date = new Date().toISOString().substr(0, 10);
+    return (
+        <div>{date}</div>
+    )
+}
+
+function Notices({ que, anw }: { que: string, anw: string }) {
     
     const [visible, setVisible] = useState<boolean>(true);
-    const date = new Date().toISOString().substr(0, 10);
 
     return (
         visible ?
@@ -16,8 +22,8 @@ function Notions({ que, anw }: { que: string, anw: string }) {
                 <div className={styles.notionBtn}>
                     <FontAwesomeIcon icon={faCaretDown} onClick={() => setVisible(false)} />
                 </div>
-            </div >)
-            :
+            </div >) :
+
             (<div className={styles.notionBox2Wrapper}>
                 <div className={styles.notionBox2}>
                     <div>{que}</div>
@@ -25,7 +31,7 @@ function Notions({ que, anw }: { que: string, anw: string }) {
                 </div>
                 <div className={styles.notionInforWrapper}>
                     <div className={styles.notionDaterWrapper}>
-                        <div>{date}</div>
+                        <Day/>
                     </div>
                     <div className={styles.notionInfor}>{anw}</div>
                 </div>
@@ -42,11 +48,18 @@ export default function Notion() {
                 <section className={styles.notionSection}>
                     <div className={styles.notionWrapper}>
                         <div className={styles.notionTitle}>공지사항</div>
-                        <Notions que="배송은 언제 오나요?" anw="배송은 주문 일 기준으로 이틀 뒤 발송시작되며 배송이 시작 된 이후로는 택배사에 문의하셔야 합니다." />
-                        <Notions que="상품을 교환/반품하고 싶어요." anw="알아서 하십쇼." />
-                        <Notions que="교환/반품을 철회하고 싶어요." anw="알아서 하십쇼." />
-                        <Notions que="회원탈퇴는 어떻게 하나요?" anw="불가합니다." />
-                        <Notions que="배송 받은 상품이 파손되었어요." anw="알아서 하십쇼." />
+                        <Notices que="배송은 언제 오나요?"
+                            anw="배송은 주문 일 기준으로 하루 뒤 발송시작되며 배송이 시작 된 이후로는 택배사에 문의하셔야 합니다." />
+                        <Notices que="상품을 교환/반품하고 싶어요."
+                            anw="[교환/반품 신청 기간]
+                            교환/취소/반품/교환/환불은 배송 완료 후 7일 이내에 가능합니다.
+                            고객님이 받으신 상품의 내용이 표시 광고 및 계약 내용과 다른 경우 상품을 수령하신 날로부터 3개월 이내,
+                            그 사실을 안 날(알 수 있었던 날)부터30일 이내에 신청이 가능합니다." />
+                        <Notices que="교환/반품을 철회하고 싶어요." 
+                            anw="반품 요청 후 반송 물품을 발송 하기전 상태인 '반품 요청중' 상태라면
+                            구매 내역 페이지나 반품 정보 페이지에서 반품 철회가 가능합니다." />
+                        <Notices que="회원탈퇴는 어떻게 하나요?" anw="불가합니다." />
+                        <Notices que="배송 받은 상품이 파손되었어요." anw="알아서 하십쇼." />
                     </div>
                 </section>
             </div>
