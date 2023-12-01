9c8c29d6c341be3d56184db2cef8e23e3c8f3b7b
bliss1454 <rkgus1454@naver.com>
2023-12-01 15:01:46 +0900

    [Add] evnet header

--- src/route/Event.tsx
+++ src/route/Event.tsx
@@ -8,22 +8,22 @@ export default function Event() {
         <h1 className={event.demure}>Demure</h1>
         <div>
           <Link to="/event/Christmas" className={event.link} style={{ textDecoration: 'none' }}>
-            <div className={event.linkTitle}>크리스마스</div>
+            <div className={event.linkTitle}>크리스마스 쿠폰</div>
           </Link>
         </div>
         <div>
           <Link to="/event/Coupon" className={event.link} style={{ textDecoration: 'none' }}>
-            <div className={event.linkTitle}>쿠폰기획전</div>
+            <div className={event.linkTitle}>크리스마스 기획전</div>
           </Link>
         </div>
         <div>
           <Link to="/event/Newyear" className={event.link} style={{ textDecoration: 'none' }}>
-            <div className={event.linkTitle}>신년특가</div>
+            <div className={event.linkTitle}>신학기 얼리버드 이벤트</div>
           </Link>
         </div>
         <div>
           <Link to="/event/First" className={event.link} style={{ textDecoration: 'none' }}>
-            <div className={event.linkTitle}>카드할인사</div>
+            <div className={event.linkTitle}>첫 구매 할인</div>
           </Link>
         </div>
       </div>
