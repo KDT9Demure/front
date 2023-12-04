c631355da08e4d824f747850f8fe8f0f07827aad
hdh9078 <hdh9071@naver.com>
2023-12-04 15:54:34 +0900

    [Feat] login page 엔터 로그인 기능 [Design] 버튼 hover

--- src/route/signin.tsx
+++ src/route/signin.tsx
@@ -52,7 +52,9 @@ export default function Signin() {
                 <div className={styles.wrapper}>
                     <div className={styles.main}>
                         <input className={styles.id} name="id" type="id" placeholder="ID" onChange={e => { setId(e.target.value); }} />
-                        <input className={styles.password} name="password" type="password" placeholder="Password" onChange={e => { setPw(e.target.value); }} />
+                        <input className={styles.password} name="password" type="password" placeholder="Password" onChange={e => { setPw(e.target.value) }} onKeyDown={(e) => {
+                            if(e.key === "Enter") {signinData()} //enter키로 로그인
+                        }}/>
                         <button className={styles.signinBtn} type="button" onClick={signinData}>로그인</button>
                         <button className={styles.kakaoBtn} onClick={kakaoSignIn}>카카오로 로그인</button>
                     </div>
