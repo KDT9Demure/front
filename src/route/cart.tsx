96770ed1c9ba740d74e5dce7ff0cb983de2dcf0c
hdh9078 <hdh9071@naver.com>
2023-12-07 20:53:50 +0900

    [HotFix] cart page 체크 없이 삭제되는 것

--- src/route/cart.tsx
+++ src/route/cart.tsx
@@ -107,6 +107,10 @@ export default function Cart() {
     }, [])
 
     const Delete = () => {
+        if (checkedIds.length === 0) {
+            alert("상품을 선택해주세요.");
+            return;
+        }
         if (!confirm('제품을 삭제하시겠습니까?')) {
             return;
         }
